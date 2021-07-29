07b1122032b09aaac71e737c3bf8ec5001ccdcb2
liyt <liyongtaoty@163.com>
2021-07-29 21:11:31 +0800

    处理404级别的错误

---
+++ src/main/java/com/basic/controller/common/CustomErrorController.java
@@ -0,0 +1,50 @@
+package com.basic.controller.common;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+class CustomErrorController extends BasicErrorController {
+    public CustomErrorController(ServerProperties serverProperties) {
+        super(new DefaultErrorAttributes(), serverProperties.getError());
+    }
+
+    //通过浏览器访问的url 如果发生异常全部会被浏览并跳转到list.html页面
+    @Override
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        //请求的状态
+        HttpStatus status = getStatus(request);
+        response.setStatus(getStatus(request).value());
+
+        Map<String, Object> model = getErrorAttributes(request,
+                isIncludeStackTrace(request, MediaType.TEXT_HTML));
+        ModelAndView modelAndView = resolveErrorView(request, response, status, model);
+        //指定自定义的视图
+        return (modelAndView == null ? new ModelAndView("404", model) : modelAndView);
+    }
+
+    //通过http client访问的接口如果发生异常会调用这个方法
+    @Override
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+
+        //输出自定义的Json格式
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("success", false);
+        map.put("code", "10001");
+        map.put("message", "请求地址错误或页面不存在");
+
+        return new ResponseEntity<Map<String, Object>>(map, status);
+    }
+}
--- src/main/resources/templates/404.ftl
+++ src/main/resources/templates/404.ftl
@@ -5,9 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>页面不存在或已被删除</title>
+    <title>错误页面</title>
+    <style>
+        body {
+            padding: 0px;
+            margin: 0px;
+            background: #f3f3f4;
+            position: relative;
+            width: 100%;
+            height: 100vh;
+            color: #1890ff;
+        }
+        .error-box {
+            position: absolute;
+            top: 30%;
+            left: 50%;
+            transform: translateY(-50%) translateX(-50%);
+        }
+
+    </style>
 </head>
 <body>
-404-页面不存在或已被删除
+<div class="error-box">
+    <p style="font-size: 24px; color: #FF9800;">页面出错啦！</p>
+    <p >错误编码：404</p>
+    <p >错误信息：请求地址错误或页面不存在！</p>
+</div>
+
 </body>
 </html>
