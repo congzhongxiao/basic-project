5b4b4c683bf45f9d291236633c9fbdc4eaebb657
leeatao <liyongtaoty@163.com>
2021-08-31 21:55:01 +0800

    日志配置输出

---
+++ src/main/java/com/basic/common/aspect/WebLogAspect.java
@@ -0,0 +1,88 @@
+package com.basic.common.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+@Aspect
+@Component
+public class WebLogAspect {
+    /**
+     * 以 controller 包下定义的所有请求为切入点
+     */
+    @Pointcut("execution(public * com.basic.controller..*.*(..))")
+    public void webLog() {
+    }
+
+    /**
+     * 在切点之前
+     *
+     * @param joinPoint
+     */
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 打印请求相关参数
+        log.info("========================================== Start ==========================================");
+        // 打印请求 url
+        log.info("URL            : {}", request.getRequestURL().toString());
+        // 打印 Http method
+        log.info("HTTP Method    : {}", request.getMethod());
+        // 打印调用 controller 的全路径以及执行方法
+        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        // 打印请求的 IP
+        log.info("IP             : {}", request.getRemoteAddr());
+        // 打印请求入参
+        Object[] args = joinPoint.getArgs();
+        Stream<?> stream = ArrayUtils.isEmpty(args) ? Stream.empty() : Arrays.stream(args);
+        List<Object> logArgs = stream
+                .filter(arg -> (!(arg instanceof HttpServletRequest) && !(arg instanceof HttpServletResponse)))
+                .collect(Collectors.toList());
+        log.info("Request Args   : {}", JSONObject.toJSONString(logArgs));
+    }
+
+    /**
+     * 在切点之后
+     */
+    @After("webLog()")
+    public void doAfter() {
+        log.info("=========================================== End ===========================================");
+        // 每个请求之间空一行
+        log.info("");
+    }
+
+    /**
+     * 环绕
+     *
+     * @param proceedingJoinPoint
+     * @throws Throwable
+     */
+    @Around("webLog()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 打印出参
+
+        log.info("Response Args  : {}", result);
+        // 执行耗时
+        log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
+        return result;
+    }
+}
