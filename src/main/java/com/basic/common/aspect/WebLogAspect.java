eb34cc6748b176efa62c6d8819cb6b0b6f7f7a1f
男爵baby <5167196+workbaby@user.noreply.gitee.com>
2021-09-02 13:38:15 +0800

    修改请求日志输出

--- src/main/java/com/basic/common/aspect/WebLogAspect.java
+++ src/main/java/com/basic/common/aspect/WebLogAspect.java
@@ -1,21 +1,15 @@
 package com.basic.common.aspect;
 
-import com.alibaba.fastjson.JSONObject;
+import com.basic.common.json.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ArrayUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
 
 @Slf4j
 @Aspect
@@ -34,28 +28,29 @@ public class WebLogAspect {
      * @param joinPoint
      */
     @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) {
-        // 开始打印请求日志
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
+    public void doBefore(JoinPoint joinPoint){
+        try{
+            // 开始打印请求日志
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = attributes.getRequest();
 
-        // 打印请求相关参数
-        log.info("========================================== Start ==========================================");
-        // 打印请求 url
-        log.info("URL            : {}", request.getRequestURL().toString());
-        // 打印 Http method
-        log.info("HTTP Method    : {}", request.getMethod());
-        // 打印调用 controller 的全路径以及执行方法
-        log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-        // 打印请求的 IP
-        log.info("IP             : {}", request.getRemoteAddr());
-        // 打印请求入参
-        Object[] args = joinPoint.getArgs();
-        Stream<?> stream = ArrayUtils.isEmpty(args) ? Stream.empty() : Arrays.stream(args);
-        List<Object> logArgs = stream
-                .filter(arg -> (!(arg instanceof HttpServletRequest) && !(arg instanceof HttpServletResponse)))
-                .collect(Collectors.toList());
-        log.info("Request Args   : {}", JSONObject.toJSONString(logArgs));
+            // 打印请求相关参数
+            log.info("========================================== Start ==========================================");
+            // 打印请求 url
+            log.info("URL            : {}", request.getRequestURL().toString());
+            // 打印 Http method
+            log.info("HTTP Method    : {}", request.getMethod());
+            // 打印调用 controller 的全路径以及执行方法
+            log.info("Class Method   : {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            // 打印请求的 IP
+            log.info("IP             : {}", request.getRemoteAddr());
+            // 打印请求入参
+            Map<String, String[]> map = request.getParameterMap();
+            String params = JSON.marshal(map);
+            log.info("params             : {}", params);
+        }catch (Exception e){
+
+        }
     }
 
     /**
@@ -74,13 +69,6 @@ public class WebLogAspect {
      */
     @Around("webLog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        Object result = proceedingJoinPoint.proceed();
-        // 打印出参
-
-        log.info("Response Args  : {}", result);
-        // 执行耗时
-        log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
-        return result;
+        return proceedingJoinPoint.proceed();
     }
 }
