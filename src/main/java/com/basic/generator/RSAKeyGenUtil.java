67abcb8ef19faec1b11050d423cfdc8636e3b9fb
liyt <liyongtaoty@163.com>
2021-12-15 19:00:34 +0800

    回退RSA公钥解密机制

--- src/main/java/com/basic/generator/RSAKeyGenUtil.java
+++ src/main/java/com/basic/generator/RSAKeyGenUtil.java
@@ -20,15 +20,15 @@ public class RSAKeyGenUtil {
         } else {
             System.out.println("生成私钥失败！");
         }*/
-       demo();
+//       demo();
     }
 
     public static void demo() throws Exception {
         String source = "abcdefghigklmnopqrstuvwxyz1234567890你好么";
         //公钥加密
-        String encryptRes = RSAUtil.encryptByPrivateKey(source, UserConstant.LOGIN_RSA_PRIVATE_KEY);
+        String encryptRes = RSAUtil.encrypt(source, UserConstant.LOGIN_RSA_PUBLIC_KEY);
         System.out.println("加密后的内容:\r\n" + encryptRes);
-        System.out.println("解密后的内容:\r\n" + RSAUtil.decryptByPublicKey(encryptRes, UserConstant.LOGIN_RSA_PUBLIC_KEY));
+        System.out.println("解密后的内容:\r\n" + RSAUtil.decrypt(encryptRes, UserConstant.LOGIN_RSA_PRIVATE_KEY));
 
     }
 }
