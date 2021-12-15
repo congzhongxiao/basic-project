81b4be9f1da03566d20ac1e8f54575adbcbd1cd9
liyt <liyongtaoty@163.com>
2021-12-15 16:52:40 +0800

    蓝色主题样式修改

--- src/main/java/com/basic/generator/RSAKeyGenUtil.java
+++ src/main/java/com/basic/generator/RSAKeyGenUtil.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class RSAKeyGenUtil {
     public static void main(String[] args) throws Exception {
-        Map<String, Object> keyMap = RSAUtil.genKeyPair();
+       /* Map<String, Object> keyMap = RSAUtil.genKeyPair();
         Object publicObj = keyMap.get(RSAUtil.PUBLIC_KEY_ENCODED);
         if (publicObj != null) {
             System.out.println("生成的公钥为：\r\n" + publicObj.toString());
@@ -19,15 +19,16 @@ public class RSAKeyGenUtil {
             System.out.println("生成的私钥为：\r\n" + privateObj.toString());
         } else {
             System.out.println("生成私钥失败！");
-        }
+        }*/
+       demo();
     }
 
     public static void demo() throws Exception {
         String source = "abcdefghigklmnopqrstuvwxyz1234567890你好么";
         //公钥加密
-        String encryptRes = RSAUtil.encrypt(source, UserConstant.LOGIN_RSA_PUBLIC_KEY);
+        String encryptRes = RSAUtil.encryptByPrivateKey(source, UserConstant.LOGIN_RSA_PRIVATE_KEY);
         System.out.println("加密后的内容:\r\n" + encryptRes);
-        System.out.println("解密后的内容:\r\n" + RSAUtil.decrypt(encryptRes, UserConstant.LOGIN_RSA_PRIVATE_KEY));
+        System.out.println("解密后的内容:\r\n" + RSAUtil.decryptByPublicKey(encryptRes, UserConstant.LOGIN_RSA_PUBLIC_KEY));
 
     }
 }
